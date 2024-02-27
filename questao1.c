65c4587fa2f9b481f439579553f86769f178c57f
deborabckp <debora.costa22396@alunos.ufersa.edu.br>
2024-02-27 15:20:55 -0300

    Primeira questão

---
+++ questao1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*1. Uma aplicação interessante de computadores é desenhar gráficos e gráficos de barras. Escreva um
+programa que leia cinco números (cada um entre 1 e 30). Para cada número lido, seu programa
+deve imprimir uma linha contendo aquele número de asteriscos adjacentes. Por exemplo, se seu
+programa lê o número sete, ele deve imprimir *******.*/
+
+int main(){
+    int i, num;
+
+    printf("Digite 5 numeros de 1 a 30:");
+    for( i = 0; i < 5; i++){
+        printf("\n");
+        scanf("%i", &num);
+
+    for(int j = 0; j < num; j++){
+        printf("*");
+    }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
